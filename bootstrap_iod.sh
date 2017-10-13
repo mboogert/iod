41f7a7ce98f9feaed2075971ace2a7d84f27798f
Marcel Boogert <marcel@mtdb.nl>
2017-10-13 23:01:38 +0200

    Update bootstrap_iod.sh

--- bootstrap_iod.sh
+++ bootstrap_iod.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd /
+
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum -y install docker-ce
+yum -y install docker-ce docker-compose
 yum -y install git
 
 systemctl enable docker
@@ -11,4 +13,4 @@ systemctl start docker
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone https://github.com/mboogert/iod.git
 
-docker-compose up -d
+cd /iod ; docker-compose up -d
