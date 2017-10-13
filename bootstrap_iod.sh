5207b918249e5fbfbdcb6c1608af2416562a35cb
Marcel Boogert <marcel@mtdb.nl>
2017-10-13 23:27:38 +0200

    Update bootstrap_iod.sh

--- bootstrap_iod.sh
+++ bootstrap_iod.sh
@@ -14,3 +14,7 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone https://github.com/mboogert/iod.git
 
 cd /iod ; docker-compose up -d
+
+sleep 30
+
+curl -X POST -d '{"name":"influxdb", "type":"influxdb", "url":"http://influxdb:8086", "access":"proxy", "database":"telegraf", "basicAuth":false}' -H 'Content-Type: application/json;charset=UTF-8' http://admin:admin@127.0.0.1/api/datasources/
