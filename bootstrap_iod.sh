723741fae838adcd91906fd9cbcf8d73d9dcb7b5
Marcel Boogert <marcel@mtdb.nl>
2017-10-13 22:02:52 +0200

    Create bootstrap_iod.sh

---
+++ bootstrap_iod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
+yum -y install git
+
+systemctl enable docker
+systemctl start docker
+
+git clone git@github.com:mboogert/iod.git
+docker-compose up -d
