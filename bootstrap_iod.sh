18817c243bd89014233276629f90a06bec9db6f5
Marcel Boogert <mboogert@proserve.nl>
2017-10-26 09:15:55 +0200

    updated some files

--- bootstrap_iod.sh
+++ bootstrap_iod.sh
@@ -24,4 +24,5 @@ do
 done
 
 curl -X POST -d '{"name":"influxdb", "type":"influxdb", "url":"http://influxdb:8086", "access":"proxy", "database":"telegraf", "basicAuth":false}' -H 'Content-Type: application/json;charset=UTF-8' http://admin:admin@localhost/api/datasources/
-curl -X POST -d @/iod/telegraf-system-dashboard.json -H 'Content-Type: application/json;charset=UTF-8' http://admin:admin@localhost/api/dashboards/db
+curl -X POST -d @/iod/linux-dashboard.json -H 'Content-Type: application/json;charset=UTF-8' http://admin:admin@localhost/api/dashboards/db
+curl -X POST -d @/iod/windows-dashboard.json -H 'Content-Type: application/json;charset=UTF-8' http://admin:admin@localhost/api/dashboards/db
